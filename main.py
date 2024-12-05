d99332557ba1fcab40f6d5ca26a34944f98bff7b
Luis de Moura <n.moura@aluno.ifsp.edu.br>
2024-12-05 11:47:25 -0300

    Update main.py
    Correção main.py

--- main.py
+++ main.py
@@ -2,14 +2,23 @@ import aluno as a;
 import turma as t;
 
 alunos = []
+
 alunos.append(a.Aluno('Fabio', 'Teixeira', 8));
 alunos.append(a.Aluno('Fabiano', 'Teixeira', 10));
-alunos.append(a.Aluno('Melissa', 'Teixeira', -1));
+
+try:
+  alunos.append(a.Aluno('Melissa', 'Teixeira', -1))
+except ValueError as e:
+  print(f"Erro ao adicionar aluno: {e}")
 
 turmaObject = t.Turma();
 turmaObject.cadastrarAlunos(alunos);
 
 turmaObject.mostrarAlunos();
+
 print('*'*30)
-print('Aluno com menor nota:', turmaObject.menorNota.mostrarAluno())
-print('Aluno com maior nota:', turmaObject.maiorNota.mostrarAluno())
+
+if turmaObject.menorNota:
+  print('Aluno com menor nota:', turmaObject.menorNota.mostrarAluno())
+if turmaObject.maiorNota:
+  print('Aluno com maior nota:', turmaObject.maiorNota.mostrarAluno())
