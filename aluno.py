24c7a6322392935a67093f3742d47ffce04815f5
Luis de Moura <n.moura@aluno.ifsp.edu.br>
2024-12-05 11:44:55 -0300

    Update aluno.py
    correção aluno.py

--- aluno.py
+++ aluno.py
@@ -1,8 +1,11 @@
 class Aluno:
-  def __init__(self, nome, sobrenome, nota):
+  def __init__(self, nome, sobrenome, nota): 
+    if nota < 0 or nota > 10:
+      raise ValueError("A nota não pode ser menor do que 0 ou maior do que 10.")
+
     self.nome = nome;
     self.sobrenome = sobrenome;
     self.nota = nota;
 
   def mostrarAluno(self):
-    return 'Aluno: ' + self.nome + ' ' + self.sobrenome + ' - Nota: ' + str(self.nota);
+    return f'Aluno: {self.nome} {self.sobrenome} - Nota: {self.nota}'
